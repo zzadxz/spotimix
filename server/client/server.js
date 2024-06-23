ea6ede7782364c881400fc7384d887d9ef673b1b
Kiarash Sotoudeh <kiarashsotoudeh@gmail.com>
2024-06-23 16:08:00 -0400

    First commit

---
+++ server/client/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+  res.send('Spotimix Backend');
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
